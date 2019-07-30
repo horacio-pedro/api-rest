22db5af34b18c938feda708a0aae5d71c94a0562
Horácio Pedro <46960527+horacio-pedro@users.noreply.github.com>
2019-07-30 13:21:49 +0100

    commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require('cors')
 const mongoose = require("mongoose")
 const requireDir = require("require-dir")
 
 // App Start
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // DataBase
     // Mongoose
--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -4,7 +4,8 @@ const Product = mongoose.model("Product")
 
 module.exports = {
     async index(req, res) {
-        const products = await Product.find()
+        const { page = 1 } = req.query
+        const products = await Product.paginate({}, { page, limit: 10 })
 
         return res.json(products)
     },
@@ -30,7 +31,7 @@ module.exports = {
     },
 
     async destroy(req, res) {
-        await Product.findByIdAndRemove(req.params.id)
+        await Product.findByIdAndDelete(req.params.id)
 
         return res.send()
     }
